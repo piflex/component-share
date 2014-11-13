df3178114248ba2f5440298087a02c9b892952f3
Igor Pantović <igor.pantovic@boutsourcing.com>
2014-11-13 14:05:47 +0100

    ImageResizer: Frame support when using scaleToFit strategy

--- src/BWC/Share/Image/ImageResizer.php
+++ src/BWC/Share/Image/ImageResizer.php
@@ -22,23 +22,34 @@ class ImageResizer
         return $contents;
     }
 
+    /**
+     * @param int $width
+     * @param int $height
+     * @param bool|string $force If true/'force', image will be forced to resize even if it's larger then original
+     *                           If 'frame', image will contain a transparent frame if larger then original
+     *                           If false, image will not be resized if it's larger than original
+     *                             
+     */
     public function scaleToFit($width, $height, $force = false)
     {
-        $this->scale($width, $height, true, $force);
+        $this->scale($width, $height, true, (bool) $force, 'frame' === $force);
     }
 
     public function scaleToCover($width, $height, $force = false)
     {
-        $this->scale($width, $height, false, $force);
+        $this->scale($width, $height, false, $force, false);
     }
 
     /**
      * @param int $width Target width
      * @param int $height Target height
      * @param bool $toFit If true, image fill fit to given dimensions, if false, it will cover them
-     * @param bool $force If true, image will be resized even if target dimensions are larger than original
+     * @param bool $force If true, image will be resized even if target dimensions are larger than original.
+     * @param bool $frame If true, image will contain a transparent frame if 
+     *                    toFit is enabled and it's larger than original
+     *                            
      */
-    protected function scale($width, $height, $toFit, $force)
+    protected function scale($width, $height, $toFit, $force, $frame)
     {
         if (null === $this->_image) return;
 
@@ -54,20 +65,43 @@ class ImageResizer
             $scalingFactor = min($widthOver, $heightOver);
         }
 
-        if ($scalingFactor > 1 || $force) {
+        if ($scalingFactor < 1 && $toFit && $force && $frame) {
+            $destImage = $this->_createEmptyImage($width, $height);
+
+            imagecopyresampled(
+                $destImage, 
+                $this->_image,
+                ($width - $rawWidth) / 2, 
+                ($height - $rawHeight) / 2, 
+                0, 
+                0, 
+                $rawWidth, 
+                $rawHeight, 
+                $rawWidth, 
+                $rawHeight
+            );                        
+
+            $this->_image = $destImage;
+        } elseif ($scalingFactor > 1 || $force) {
             $destWidth  = $rawWidth / $scalingFactor;
             $destHeight = $rawHeight / $scalingFactor;
-
-            $destImage = imagecreatetruecolor($destWidth, $destHeight);
-            imagealphablending($destImage, false);
-            imagesavealpha($destImage, true);
-            $transparent = imagecolorallocatealpha($destImage, 255, 255, 255, 127);
-            imagefill($destImage, 0, 0, $transparent);
+            $destImage = $this->_createEmptyImage($destWidth, $destHeight);
 
             imagecopyresampled($destImage, $this->_image, 0, 0, 0, 0, $destWidth, $destHeight, $rawWidth, $rawHeight);
 
             $this->_image = $destImage;
         }
+    }
+
+    protected function _createEmptyImage($width, $height)
+    {
+        $image = imagecreatetruecolor($width, $height);
+        imagealphablending($image, false);
+        imagesavealpha($image, true);
+        $transparent = imagecolorallocatealpha($image, 255, 255, 255, 127);
+        imagefill($image, 0, 0, $transparent);        
+
+        return $image;
     }
 
     protected function _getWidth()
